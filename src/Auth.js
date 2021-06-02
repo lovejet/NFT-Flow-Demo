d02f802e29a400998df623e14b6402bc56dde5b9
Harold <haroldhong.love@gmail.com>
2021-06-02 04:20:55 -0500

    add auth

---
+++ src/Auth.js
@@ -0,0 +1,12 @@
+// File: ./src/Auth.js
+
+import React from "react"
+import {AuthCluster} from "./auth-cluster"
+
+export default function App() {
+  return (
+    <div>
+      <AuthCluster />
+    </div>
+  )
+}
